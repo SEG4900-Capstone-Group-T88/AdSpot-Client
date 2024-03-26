279d97666fdb833af7eacfe705b2c535871cc0a4
Akarsh Gharge <akarshgharge20@gmail.com>
2024-03-26 14:03:47 -0400

    Removed nested <a> elements

--- src/pages/ConnectedAccounts.tsx
+++ src/pages/ConnectedAccounts.tsx
@@ -14,18 +14,10 @@ function ConnectedAccounts() {
                 <div>
                     <h2>Connect a Social Media Account</h2>
                     <div className="flex items-center my-3">
-                        <a className="instagram-connect mx-5">
-                            <SocialIcon network="instagram" url="https://api.instagram.com/oauth/authorize?client_id=3649610268617265&redirect_uri=https://localhost:5173&scope=user_profile,user_media&response_type=code"/>
-                        </a>
-                        <a className="twitter-connect mx-5">
-                            <SocialIcon network="twitter"/>
-                        </a>
-                        <a className="youtube-connect mx-5">
-                            <SocialIcon network="youtube"/>
-                        </a>
-                        <a className="facebook-connect mx-5">
-                            <SocialIcon network="facebook" url="https://www.facebook.com/v19.0/dialog/oauth?client_id=948021529632543&redirect_uri=https://localhost:5173" />
-                        </a>
+                            <SocialIcon className="mx-5" network="instagram" url="https://api.instagram.com/oauth/authorize?client_id=3649610268617265&redirect_uri=https://localhost:5173/settings/connectInstagramAccount&scope=user_profile,user_media&response_type=code"/>
+                            <SocialIcon className="mx-5" network="twitter"/>
+                            <SocialIcon className="mx-5" network="youtube"/>
+                            <SocialIcon className="mx-5" network="facebook" url="https://www.facebook.com/v19.0/dialog/oauth?client_id=948021529632543&redirect_uri=https://localhost:5173" />
                     </div>
                 </div>
             </div>
