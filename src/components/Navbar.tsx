7cb0bf2c2623284cf86aa4f21ccda066a8b093d0
Matthew Sia <matthewsia98@gmail.com>
2024-07-14 11:10:34 -0400

    refactor create listing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,67 +1,15 @@
-import React, {useContext, useState} from 'react'
+import {useContext, useState} from 'react'
 import {useNavigate, NavLink, Link} from 'react-router-dom'
 import Profile from '../images/profile.png'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faGear} from '@fortawesome/free-solid-svg-icons'
-import {Card, Input, Checkbox, Typography, Select, Option} from '@material-tailwind/react'
 import {UserContext} from './UserContext'
-import {graphql} from '../gql'
-import {useMutation, useQuery} from 'urql'
-
-export const GetUserConnections = graphql(`
-    query GetUserConnections($input: Int!) {
-        userById(userId: $input) {
-            connections {
-                platformId
-                handle
-            }
-        }
-    }
-`)
-
-export const GetListingTypes = graphql(`
-    query GetListingTypes {
-        platforms {
-            platformId
-            name
-            listingTypes {
-                name
-                listingTypeId
-            }
-        }
-    }
-`)
-
-export const CreateListingMutation = graphql(`
-    mutation AddListing($input: AddListingInput!) {
-        addListing(input: $input) {
-            listing {
-                listingId
-                listingType {
-                    listingTypeId
-                    name
-                    platform {
-                        platformId
-                        name
-                    }
-                }
-            }
-        }
-    }
-`)
-
-interface Listing {
-    platformId: number
-    listingTypeId: number
-    name: string
-}
+import CreateListing from './CreateListing'
 
 function Navbar() {
     const navigate = useNavigate()
     const [showPopup, setShowPopup] = useState(false)
-    const [isModalOpen, setIsModalOpen] = useState(false)
     const {user, setUser} = useContext(UserContext)
-    const [, addListing] = useMutation(CreateListingMutation)
 
     const togglePopup = () => {
         setShowPopup(!showPopup)
@@ -78,14 +26,6 @@ function Navbar() {
 
     const redirectToSearch = () => {
         navigate('/search')
-    }
-
-    const openModal = () => {
-        setIsModalOpen(true)
-    }
-
-    const closeModal = () => {
-        setIsModalOpen(false)
     }
 
     function userIcon() {
@@ -107,66 +47,6 @@ function Navbar() {
                 />
             )
         }
-    }
-
-    const [userConnections] = useQuery({
-        query: GetUserConnections,
-        variables: {input: user?.userId ?? -1},
-    })
-
-    const connectedAccountsPlatformId: number[] = []
-    userConnections.data?.userById?.connections.forEach((element) => {
-        connectedAccountsPlatformId.push(element.platformId)
-    })
-
-    const [listingTypes] = useQuery({
-        query: GetListingTypes,
-    })
-
-    const listings: Listing[] = []
-
-    listingTypes.data?.platforms.forEach((element) => {
-        const platformName = element.name
-        const platformId = element.platformId
-        element.listingTypes.forEach((element) => {
-            const listingTypeId = element.listingTypeId
-            const listingName = platformName + ' ' + element.name
-            if (connectedAccountsPlatformId.includes(platformId)) {
-                listings.push({
-                    platformId: platformId,
-                    listingTypeId: listingTypeId,
-                    name: listingName,
-                } as Listing)
-            }
-        })
-    })
-
-    const createListing = (event: React.FormEvent) => {
-        event.preventDefault()
-        const listingElement = document.getElementById('listingSelection') as HTMLSelectElement
-        const listingSelection = listingElement.firstElementChild?.getAttribute('value')
-        const listingArray = listingSelection?.split(',') ?? ['0', '0']
-
-        const listingTypeId = Number(listingArray[0])
-
-        const data = new FormData(event.target as HTMLFormElement)
-        const price = Number(data.get('price'))
-
-        const userId = user?.userId ?? -1
-
-        addListing({
-            input: {
-                userId,
-                listingTypeId,
-                price,
-            },
-        }).then((result) => {
-            if (result.error) {
-                console.log('ERROR WITH LISTING CREATION!')
-            } else {
-                closeModal()
-            }
-        })
     }
 
     return (
@@ -207,14 +87,7 @@ function Navbar() {
                 >
                     Search
                 </button>
-                {user && (
-                    <button
-                        className="bg-purple text-[white] rounded-lg px-6 py-1 m-1"
-                        onClick={() => openModal()}
-                    >
-                        Post +
-                    </button>
-                )}
+                <CreateListing />
                 <div className="relative">
                     {userIcon()}
                     {showPopup && (
@@ -248,104 +121,6 @@ function Navbar() {
                     )}
                 </div>
             </div>
-            {isModalOpen && (
-                <div className="modal-bg">
-                    <div className="modal-content relative">
-                        <button
-                            className="absolute top-0 right-0 mt-4 mr-4 text-[red] text-lg font-semibold hover:text-red-700"
-                            onClick={closeModal}
-                        >
-                            &times;
-                        </button>
-                        <h2 className="text-3xl font-semibold">Create listing</h2>
-                        <Card
-                            color="transparent"
-                            placeholder=""
-                        >
-                            <form
-                                className="mb-2 w-80 max-w-screen-lg sm:w-96 flex flex-col gap-4"
-                                onSubmit={createListing}
-                            >
-                                <Typography
-                                    variant="h6"
-                                    color="blue-gray"
-                                    className="-mb-3"
-                                    placeholder=""
-                                >
-                                    Listing Type
-                                </Typography>
-                                <Select
-                                    placeholder=""
-                                    className="!border-t-blue-gray-200 focus:!border-t-gray-900 flex items-center"
-                                    labelProps={{
-                                        className: 'before:content-none after:content-none',
-                                    }}
-                                    name="listingSelection"
-                                    id="listingSelection"
-                                >
-                                    {listings.map((listing) => (
-                                        <Option
-                                            key={listing.name}
-                                            value={
-                                                listing.listingTypeId.toString() +
-                                                ',' +
-                                                listing.platformId.toString()
-                                            }
-                                        >
-                                            {listing.name}
-                                        </Option>
-                                    ))}
-                                </Select>
-                                <Typography
-                                    variant="h6"
-                                    color="blue-gray"
-                                    className="-mb-3"
-                                    placeholder=""
-                                >
-                                    Price
-                                </Typography>
-                                <Input
-                                    size="lg"
-                                    placeholder="$"
-                                    className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                                    labelProps={{
-                                        className: 'before:content-none after:content-none',
-                                    }}
-                                    crossOrigin={undefined}
-                                    name="price"
-                                />
-                                <Checkbox
-                                    label={
-                                        <Typography
-                                            variant="small"
-                                            color="gray"
-                                            className="flex items-center font-normal"
-                                            placeholder=""
-                                        >
-                                            I agree to the
-                                            <a
-                                                href="#"
-                                                className="font-medium transition-colors hover:text-gray-900"
-                                            >
-                                                &nbsp;Terms and Conditions
-                                            </a>
-                                        </Typography>
-                                    }
-                                    containerProps={{className: '-ml-2.5'}}
-                                    placeholder=""
-                                    crossOrigin={undefined}
-                                />
-                                <button
-                                    className="bg-purple text-white rounded-lg px-4 py-2 mt-2 shadow-lg"
-                                    type="submit"
-                                >
-                                    Post
-                                </button>
-                            </form>
-                        </Card>
-                    </div>
-                </div>
-            )}
         </div>
     )
 }
