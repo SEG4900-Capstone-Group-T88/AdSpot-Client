a4e5ad5ce5d50b73a60e936f37dc2cdaebf09bd8
Akarsh Gharge <akarshgharge20@gmail.com>
2024-10-28 08:40:28 -0400

    Vitest configuratioon changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 
 import {defineConfig} from 'vite'
+import {configDefaults} from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
@@ -9,5 +10,27 @@ export default defineConfig({
     plugins: [react(), basicSsl()],
     server: {
         open: true,
-    }, test: {environment: "jsdom", globals: true, setupFiles: "setupTests.ts"},
+    }, 
+    test: {
+        environment: "jsdom", 
+        globals: true, 
+        setupFiles: "setupTests.ts",
+        coverage: {
+            exclude: [
+                ...configDefaults.exclude,
+                '.eslintrc.cjs',
+                'codegen.ts',
+                'postcss.config.js',
+                'tailwind.config.js',
+                'src/vite-env.d.ts',
+                '**/*.test.tsx',
+                'src/OrderContextEnum.tsx',
+                'src/main.tsx',
+                'src/urqlClient.tsx',
+                'src/authStore.tsx'
+            ],
+            enabled: true,
+            reporter: ['html']
+        }
+    },
 })
