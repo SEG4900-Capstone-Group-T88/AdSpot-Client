4441fbd62cd0c646805ef033ed00fe1550a3a00e
Matthew Sia <71731492+matthewsia98@users.noreply.github.com>
2024-11-28 20:10:04 -0500

    Revert "fix subscription"

--- src/components/Orders.tsx
+++ src/components/Orders.tsx
@@ -119,6 +119,8 @@ function Orders(props: {
     const [subscriptionResult] = useSubscription({
         query: OnNewOrderSubscription,
         variables: {userId: user?.userId ?? -1},
+        // only subscribe on pending tab
+        pause: !(props.pov === OrderPov.Seller && props.status === OrderStatusEnum.Pending),
     })
     useEffect(() => {
         if (subscriptionResult.data?.onNewOrder) {
